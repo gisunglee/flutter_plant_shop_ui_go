656d8612a16e86991232c06c5faab3d5da628202
= <479lgs@naver.com>
2021-05-14 14:29:16 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_plant_shop_ui_go/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_plant_shop_ui_go
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/plant_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_plant_shop_ui_go/models/plant_model.dart';
+
+class PlantScreen extends StatefulWidget {
+  final Plant plant;
+
+  PlantScreen({this.plant});
+
+  @override
+  _PlantScreenState createState() => _PlantScreenState();
+}
+
+class _PlantScreenState extends State<PlantScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( );
+  }
+}
